ef17c215c92a46d551bd2b1657461c8a615a7b87
Ubuntu <ubuntu@ip-172-31-10-238.us-west-2.compute.internal>
2014-04-03 16:46:40 +0000

    api masking, different games in different topics concurrently

--- chessbot.rb
+++ chessbot.rb
@@ -1,17 +1,18 @@
 require 'chess'
 require 'zulip'
 
+@client = Zulip::Client.new do |config|
+  config.email_address = "YOUR BOT'S EMAIL ADDRESS"
+  config.api_key = "YOUR BOT'S API KEY"
+end
+
+load "./zulip_api_vars.rb" if File.exist?("./zulip_api_vars.rb")
+
+# @client.send_message("chessbot", ":grin:",  "chessbot")
+
 @game = Chess::Game.new
 @flip = false
 @games = {}
-
-@client = Zulip::Client.new do |config|
-  config.email_address = "chess-bot@students.hackerschool.com"
-  config.api_key = "Zzho0MVVjYG1UUXmcsS8NsncdqUBNsVR"
-end
-
-# @client.send_message("chessbot", "Hello. I am Chessbot. I respond to ```[properly formatted algebraic notation]```, ```peek```, and also ```start```. I'm pretty dumb right now, but you can use my board while I learn to play.", "chess")
-
 @pieces = { 'r' => '♜',
             'n' => '♞',
             'b' => '♝',
@@ -26,52 +27,55 @@ end
             'K' => '♔',
             '.' => '＿' }
 
-def send_board(board = Chess::Game.new.board)
+def send_board(topic, board = Chess::Game.new.board, stream)
   board = board.to_s.split("\e").reject {|e| e.length < 6}.map {|s| s.split}.each {|e| e.shift }[0..-2]
   board.map! {|e| e.map! {|p| @pieces[p] }}
   if @flip == true
+    # everything is mutable / life is transient; ephemeral.
     board.reverse!
     board.map! do |row|
       row.reverse
     end
   end
   board = board.map {|l| l.join('|') }.join("\n")
-  @client.send_message("chessbot", board,  "chessbot")
+  # @client.send_message("chessbot", board,  "chessbot")
+  @client.send_message(topic, board,  stream)
   # @client.send_private_message(board, "jeffowler@gmail.com")
 end
-
-# @client.send_message("oh noooo", ":cry:" ,"off-topic")
 
 
 @client.stream_messages do |message|
 
-  # @client.subscribe message.stream
   p message
+  puts
+  stream = "chessbottest"
+  topic = message.subject
+  @games[topic] ||= Chess::Game.new
 
   if message.sender_email != 'chess-bot@students.hackerschool.com'
     if !message.content.scan(/```.+```/).empty?
       my_move = message.content.scan(/```.+```/).join.slice(3..-4).strip
       if my_move == "start"
-        @game = Chess::Game.new if my_move == "start"
-        send_board(@game)
+        @games[topic] = Chess::Game.new if my_move == "start"
         @flip = false
+        send_board(topic, @games[topic], stream)
       elsif my_move == "peek"
-        send_board(@game)
+        send_board(topic, @games[topic], stream)
       else
         begin
-          @game.move(my_move)
+          @games[topic].move(my_move)
         rescue Chess::IllegalMoveError
-          @client.send_message("chessbot", 'That is not a legal move!', 'chessbot' )
+          # @client.send_message("chessbot", 'That is not a legal move!', 'chessbot' )
         rescue Chess::BadNotationError
-          @client.send_message("chessbot", "Malformed notation", "chessbot")
+          # @client.send_message("chessbot", "Malformed notation", "chessbot")
         else
           @flip = !@flip
-          @client.send_message("chessbot", 'Check!', "off-topic") if @game.board.check?
-          if @game.board.checkmate?
-            @client.send_message("chessbot", 'Checkmate, the game is over!', "chessbot") if @game.board.check?
+          # @client.send_message("chessbot", 'Check!', "chessbot") if @game.board.check?
+          if @games[topic].board.checkmate?
+            # @client.send_message("chessbot", 'Checkmate!', "chessbot") if @game.board.check?
           end
+        send_board(topic, @games[topic], stream)
         end
-      send_board(@game)
       end
     end
   end
