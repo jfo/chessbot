aa49a27d5743fa58f0bb9dcdfbae152c07ab2935
Ubuntu <ubuntu@ip-172-31-10-238.us-west-2.compute.internal>
2014-04-03 04:44:14 +0000

    chessbot works at chessbot:chessbot

--- chessbot.rb
+++ chessbot.rb
@@ -3,12 +3,12 @@ require 'zulip'
 
 @game = Chess::Game.new
 @flip = false
+@games = {}
 
 @client = Zulip::Client.new do |config|
   config.email_address = "chess-bot@students.hackerschool.com"
   config.api_key = "Zzho0MVVjYG1UUXmcsS8NsncdqUBNsVR"
 end
-@client.subscribe 'off-topic'
 
 # @client.send_message("chessbot", "Hello. I am Chessbot. I respond to ```[properly formatted algebraic notation]```, ```peek```, and also ```start```. I'm pretty dumb right now, but you can use my board while I learn to play.", "chess")
 
@@ -27,43 +27,52 @@ end
             '.' => '＿' }
 
 def send_board(board = Chess::Game.new.board)
-  newboard = board.to_s.split("\e").reject {|e| e.length < 6}.map {|s| s.split}.each {|e| e.shift }[0..-2]
-  newboard.map! {|e| e.map! {|p| @pieces[p] }}
+  board = board.to_s.split("\e").reject {|e| e.length < 6}.map {|s| s.split}.each {|e| e.shift }[0..-2]
+  board.map! {|e| e.map! {|p| @pieces[p] }}
   if @flip == true
-    newboard.reverse!
-    newboard.map! do |row|
+    board.reverse!
+    board.map! do |row|
       row.reverse
     end
   end
-  newboard = newboard.map {|l| l.join('|') }.join("\n")
-  @client.send_message("chessbot", newboard,  "off-topic")
-  # @client.send_private_message(newboard, "jeffowler@gmail.com")
+  board = board.map {|l| l.join('|') }.join("\n")
+  @client.send_message("chessbot", board,  "chessbot")
+  # @client.send_private_message(board, "jeffowler@gmail.com")
 end
 
-send_board
+# @client.send_message("oh noooo", ":cry:" ,"off-topic")
+
 
 @client.stream_messages do |message|
+
+  # @client.subscribe message.stream
+  p message
 
   if message.sender_email != 'chess-bot@students.hackerschool.com'
     if !message.content.scan(/```.+```/).empty?
       my_move = message.content.scan(/```.+```/).join.slice(3..-4).strip
-      @game = Chess::Game.new if my_move == "start"
-      send_board(@game) if my_move == "peek"
-      begin
-        @game.move(my_move) if my_move != 'start' && my_move != 'peek'
-      rescue Chess::IllegalMoveError
-        @client.send_message("chessbot", 'That is not a legal move!', 'off-topic' )
-      rescue Chess::BadNotationError
-        @client.send_message("chessbot", "Malformed notation", "off-topic")
+      if my_move == "start"
+        @game = Chess::Game.new if my_move == "start"
+        send_board(@game)
+        @flip = false
+      elsif my_move == "peek"
+        send_board(@game)
       else
-        @flip = !@flip
-        @client.send_message("chessbot", 'Check!', "off-topic") if @game.board.check?
-        if @game.board.checkmate?
-          @client.send_message("chessbot", 'Checkmate, the game is over! Type ```start``` to go again :)', "off-topic") if @game.board.check?
+        begin
+          @game.move(my_move)
+        rescue Chess::IllegalMoveError
+          @client.send_message("chessbot", 'That is not a legal move!', 'chessbot' )
+        rescue Chess::BadNotationError
+          @client.send_message("chessbot", "Malformed notation", "chessbot")
+        else
+          @flip = !@flip
+          @client.send_message("chessbot", 'Check!', "off-topic") if @game.board.check?
+          if @game.board.checkmate?
+            @client.send_message("chessbot", 'Checkmate, the game is over!', "chessbot") if @game.board.check?
+          end
         end
-        send_board(@game) if my_move != "peek"
+      send_board(@game)
       end
     end
   end
-
 end
