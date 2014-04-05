c802c527f05895b1c9675ddd0a6fcbfc362c049f
urthbound <jeffowler@gmail.com>
2014-04-05 17:19:41 -0400

    mid pm work

--- chessbot.rb
+++ chessbot.rb
@@ -25,7 +25,7 @@ load "./zulip_api_vars.rb" if File.exist?("./zulip_api_vars.rb")
             'K' => '♔',
             '.' => '＿' }
 
-def send_board(topic, board = Chess::Game.new.board, stream)
+def prep_board(board = Chess::Game.new.board)
   board = board.to_s.split("\e").reject {|e| e.length < 6}.map {|s| s.split}.each {|e| e.shift }[0..-2]
   board.map! {|e| e.map! {|p| @pieces[p] }}
   if @flip == true
@@ -36,12 +36,14 @@ def send_board(topic, board = Chess::Game.new.board, stream)
     end
   end
 
-  if fake
-    board.map! {|row| row.map! {|p| p == '＿' ? '♕' : p }}
-  end
+  board.map {|l| l.join('|') }.join("\n")
+end
 
-  board = board.map {|l| l.join('|') }.join("\n")
+def stream_send_board(topic, board, stream)
   @client.send_message(topic, board,  stream)
+end
+
+def pm_send_board
 end
 
 def stream_game(message)
@@ -49,40 +51,40 @@ def stream_game(message)
   topic = message.subject
 
   game_key = stream + '::' + topic
-  @games[game_key] ||= Chess::Game.new
+  @games[game_key] ||= {game:Chess::Game.new, flip:false}
 
   if message.sender_email != @client.email_address
     if !message.content.scan(/`[a-zA-Z0-9\-]+`/).empty?
       my_move = message.content.scan(/`[a-zA-Z0-9\-]+`/).join.slice(1..-2).strip
       puts my_move
       if my_move == "start"
-        @games[game_key] = Chess::Game.new
-        @flip = false
-        send_board(topic, @games[game_key], stream)
+        @games[game_key][:game] = Chess::Game.new
+        @games[game_key][:flip] = false
+        stream_send_board(topic, prep_board(@games[game_key][:game]), stream)
       elsif my_move == "peek"
-        send_board(topic, @games[game_key], stream)
+        stream_send_board(topic, prep_board(@games[game_key][:game]), stream)
       elsif my_move == "gg" || my_move == "resign"
         @client.send_message(topic, 'gg!', stream)
-        @games[game_key] = Chess::Game.new
-        @flip = false
+        @games[game_key][:game] = Chess::Game.new
+        @games[game_key][:flip] =
       elsif my_move == "halp"
         @client.send_message(topic, '[](http://blog.check-and-secure.com/wp-content/uploads/2014/02/halp.png)', stream)
       elsif my_move == "help"
-        @client.send_message(topic, "I respond to the following commands:\n```start``` sets up a new board.\n```peek``` prints out the board again\n```[any properly notated legal move]``` in [algebraic notation](http://en.wikipedia.org/wiki/Algebraic_notation_(chess)) makes the move, flips the board, and prints it out again.\n\nI listen to everything on stream 'chessbot' where every topic gets its own table.\n\nCommands must be formatted between backticks to form a markdown code block. I'm not so good at conversation, but I'm a cheerful bot if you'd like to PM me sometime!", stream)
+        @client.send_message(topic, "I respond to the following commands:\n```start``` sets up a new board.\n```peek``` prints out the board again\n```[any properly notated legal move]``` in [algebraic notation](http://en.wikipedia.org/wiki/Algebraic_notation_(chess)) makes the move, flips the board, and prints it out again.\n\nI listen to everything on stream 'chessbot' where every topic gets its own table.\n\nCommands must be formatted between backticks to form a markdown code block.", stream)
       else
         begin
-          @games[game_key].move(my_move)
+          @games[game_key][:game].move(my_move)
         rescue Chess::IllegalMoveError
           @client.send_message(topic, 'That is not a legal move!', stream)
         rescue Chess::BadNotationError
           @client.send_message(topic, 'Malformed notation.', stream)
         else
-          @flip = !@flip
-          @client.send_message(topic, 'Check!', stream) if @games[gamekey].board.check?
-          if @games[game_key].board.checkmate?
+          @games[game_key][:flip] = !@games[game_key][:flip]
+          if @games[game_key][:game].board.checkmate?
             @client.send_message(topic, 'Checkmate!', stream)
           end
-        send_board(topic, @games[game_key], stream)
+          @client.send_message(topic, 'Check!', stream) if @games[game_key][:game].board.check?
+        stream_send_board(topic, prep_board(@games[game_key][:game]), stream)
         end
       end
     end
@@ -94,15 +96,15 @@ def main
   puts "Chessbot is listening"
 
   @client.stream_messages do |message|
-    p message
-    puts
+
+    puts message
 
     if message.type == "stream"
       stream_game(message)
-    else
-      if message.sender_email != @client.email_address
-        @client.send_private_message(":grin:",  message.sender_email)
-      end
+    elsif message.type == "private"
+      # stream_game(message)
     end
   end
 end
+
+main
