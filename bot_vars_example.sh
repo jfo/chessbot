a69c465af929888586edb25e80feda767f90e47c
Jeff Fowler <jeffowler@gmail.com>
2014-04-09 15:10:18 -0400

    Update bot_vars_example.sh

--- bot_vars_example.sh
+++ bot_vars_example.sh
@@ -1,3 +1,3 @@
-# replace the below and rename this file to "bot_vars.sh" to get set up.
+# replace the below and source this file (or add it to your path) to get set up
 export chessbot_email="YOUR BOT'S EMAIL"
 export chessbot_api_key="YOUR BOT'S API KEY"
